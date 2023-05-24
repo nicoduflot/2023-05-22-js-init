2f9b48bab34dd81a56d584f8b2c84ad098368e34
unknown <nduflot@dawan.fr>
2023-05-24 10:45:09 +0200

    cookie pt 1

---
+++ js/cookies.js
@@ -0,0 +1,30 @@
+let cookieValue = 'Nico';
+/*
+let dateNow = new Date();
+console.log(dateNow);
+console.log(dateNow.getDate());
+console.log(dateNow.getMonth()+1);
+console.log(dateNow.getTime());
+console.log(dateNow.getTime() + (1 * 24 * 60 * 60 * 1000) );
+dateNow.setTime(dateNow.getTime() + (1 * 24 * 60 * 60 * 1000) );
+console.log(dateNow);
+console.log(dateNow.toLocaleString());
+console.log(dateNow.toDateString());
+console.log(dateNow.toUTCString());
+console.log(dateNow.toLocaleDateString());
+*/
+
+/*
+let valDays = 24 * 60 *60;
+
+document.cookie = `userTest=titi; max-age=${valDays}; path=/; Samesite=Strict; Secure`;
+
+console.log(document.cookie);
+*/
+
+function setCookie(name, value = '', days = -1, path = '/', samesite = 'Strict', secure = 'secure'){
+    /* il est possible d'ajouter plus de paramètres, par défaut tel que le path, samesite et secure */
+    let maxAge = days * 24 * 60 * 60;
+    document.cookie = `${name}=${value}; max-age=${maxAge}; path=${path}; Samesite=${samesite}; ${secure}`;
+}
+

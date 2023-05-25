ecb826de89616d3ac94796ce5b61e8c411f20880
unknown <nduflot@dawan.fr>
2023-05-25 16:09:28 +0200

    ajax fetch()

--- js/functions.js
+++ js/functions.js
@@ -21,3 +21,42 @@ function $(selector){
     return false;
 }
 */
+
+function jsonToList(data = {message: "pas de flux récupéré"}){
+    /* création d'un élément liste */
+    let ul = document.createElement('ul');
+    if(data.length >= 0){
+        /* si data est un tableau */
+        data.forEach(function(value, key){
+            /* un forEach sur chacun des éléments du tableau */
+            let li = document.createElement('li');
+            ul.append(li);
+            /* on envoie le contenu de la case du tableau dans la fonction en récursif */
+            li.append(jsonToList(value));
+        });        
+    }else{
+        /* si data est un objet */
+        for(key in data){
+            /* on parcour les valeur des clefs de l'objet */
+            let li = document.createElement('li');
+            if(typeof data[key] === 'object'){
+                /* 
+                si la valeur de la clef est un objet, 
+                on crée un élément de liste avec la clef puis on y ajoute le retour de la fonction en récursif 
+                */
+                li.append(document.createTextNode(`${key}`));
+                li.append(jsonToList(data[key]));
+                
+            }else{
+                /* 
+                si la valeur de la clef n'est pas un objet 
+                on crée un élément de liste avec la clef
+                */
+                li.append(document.createTextNode(`${key} : ${data[key]}`));
+            }
+            /* on ajoute ensuite le li à la liste qu'on retourne en réponse de la fonction */
+            ul.append(li);
+        }
+    }
+    return ul;
+}
